61603e21e5510c5af8af0952380f2a7afd716636
thomrick <tricart@xebia.fr>
2017-11-15 09:33:45 +0100

    update index.js to use built sources + package.json with serve command

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const server = require('./src/server');
+const server = require('./dist/server');
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("Node app is running at localhost:", PORT));
